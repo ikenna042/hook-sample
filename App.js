5d8f92a6e187cdb2e63f55e457c4a9ad526cef46
IKENNA <ikennavictorofficial@gmail.com>
2021-11-19 12:36:44 +0100

    playing with flexbox ui

--- App.js
+++ App.js
@@ -1,49 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Linking, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Linking, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const App = () => {
-  let [sum, setSum] = useState(0)
-  let [clicks, SetClicks] = useState(0)
-
-  const onPress = () => {
-    setSum(sum+=5)
-    SetClicks(clicks+=1)
-  }
+  
   return (
-    <View style={styles.container}>
-      <Text>{sum}</Text>
-      <Pressable style={styles.button} onPress={onPress}>
-        <Text style={styles.text}>Add</Text>
-      </Pressable>
-      <Text>You clicked {clicks} times</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.view1}>
+      <View style={styles.box1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.box2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.box3}>
+          <Text style={styles.text}>3</Text>
+        </View>
+      </View>
+      <View style={styles.view2}>
+        <View style={styles.box4}>
+          <Text style={styles.text}>4</Text>
+        </View>
+        <View style={styles.box5}>
+          <Text style={styles.text}>5</Text>
+        </View>
+      </View>
+      <View style={styles.view4}>
+        <View style={styles.box6}>
+          <Text style={styles.text}>6</Text>
+        </View>
+        <View style={styles.box7}>
+          <Text style={styles.text}>7</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: 'blue',
   },
   text: {
-    fontSize: 16,
+    fontSize: 20,
     lineHeight: 21,
     fontWeight: 'bold',
     letterSpacing: 0.25,
-    color: 'white',
+    color: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+  view1: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  view2: {
+    flex: 2,
+    flexDirection: 'column'
+  },
+  view4: {
+    flex: 5,
+    flexDirection: 'row'
+  },
+  box1: {
+    flex: 1,
+    backgroundColor: '#d5eacf',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  box2: {
+    flex: 2,
+    backgroundColor: '#f55da8',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  box3: {
+    flex: 3,
+    backgroundColor: '#ffdb19',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  box4: {
+    flex: 1,
+    backgroundColor: '#ff0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  box5: {
+    flex: 2,
+    backgroundColor: '#8fce00',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  box6: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },box7: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
   },
 });
 
