5744f676727a40a8638a769ca471e7a312dc3d3a
IKENNA <ikennavictorofficial@gmail.com>
2021-11-19 10:45:53 +0100

    react hooks

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Linking, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  let [sum, setSum] = useState(0)
+  let [clicks, SetClicks] = useState(0)
+
+  const onPress = () => {
+    setSum(sum+=5)
+    SetClicks(clicks+=1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{sum}</Text>
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>Add</Text>
+      </Pressable>
+      <Text>You clicked {clicks} times</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'blue',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
 });
+
+export default App;
